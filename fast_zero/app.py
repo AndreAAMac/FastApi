99c6f906d2a0106028c53c854be1d2571d534d09
Andre A A Machado <andre_almac@ouutlook.com>
2024-08-29 20:42:57 -0300

    Iniciando Projeto

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI  # type: ignore
+#Descobrir o endereço local
+#>>> import socket
+#>>> s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#>>> s.connect(("8.8.8.8", 80))
+#>>> s.getsockname()[0]
+#'192.168.0.100'
+
+from http import HTTPStatus
+
+from fastapi import FastAPI
+
+from fast_zero.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+@app.post('/users/', response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
